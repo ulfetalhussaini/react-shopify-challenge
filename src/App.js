b64182d99c46d5c02aeac141687778f41c842fba
Yasser Muayed <yassermuayed@gmail.com>
2021-08-23 19:22:54 +0300

    add useFetch custom hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import useFetch from './hooks/useFetch';
 
 function App() {
+
+  console.log(useFetch())
+  
   return (
     <div className="App">
         <h1>Just testing by yasser</h1>
