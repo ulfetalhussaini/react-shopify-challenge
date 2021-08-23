e94dc10886eb3df116b4bd116625bbb8e43f926a
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-08-23 19:29:26 +0300

    add components folder, search and nominatedList files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    borderRadius: '0'
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-        <h1>Just testing by yasser</h1>
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>Search part</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>NominatedList part</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
-
-export default App;
