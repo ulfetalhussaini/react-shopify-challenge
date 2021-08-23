4690f7c88549517174e1830b842646617f6049f4
Yasser Muayed <yassermuayed@gmail.com>
2021-08-23 18:57:48 +0300

    install axios

---
+++ src/api.js
@@ -0,0 +1,2 @@
+// key=  61b843cd
+// http://www.omdbapi.com/?i=tt3896198&apikey=61b843cd
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+const useFetch = () => {
+    
+    useEffect(()=>{
+
+    },[])
+
+}
+
+export default useFetch
